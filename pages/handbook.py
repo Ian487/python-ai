1a14c4057e44051f964451ce8c7c8cb13b040fd0
Ian487 <demon.master9487@gmail.com>
2024-07-10 17:26:59 +0800

    class3

--- pages/handbook.py
+++ pages/handbook.py
@@ -12,7 +12,7 @@ for f in files:
         files_name.append(f)  # 將檔案加到files_name中
 print(files_name)  # 印出所有的markdown檔案
 for f in files_name:  # 逐一讀取files_name中的檔案
-    with open(f"{floderpath}\{f}", "r", encoding="utf-8") as file:  # 開啟檔案
+    with open(f"{floderpath}/{f}", "r", encoding="utf-8") as file:  # 開啟檔案
         content = file.read()  # 讀取檔案內容
     with st.expander(f"{f[:-3]}"):  # 建立一個擴展元件, 標題是檔案名稱
         st.markdown(content)  # 把檔案內容顯示在擴展元件中
